7b22775e9de5a3925b58442d2212fe72a3604977
MARTIN KIMANI <96081023+Martokim@users.noreply.github.com>
2025-04-22 14:44:32 +0000

    did  the challange

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create a pet
+my_pet = Pet("Kamjus")
+
+# Interact with your pet
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.train("Play catch ")
+my_pet.show_tricks()
+my_pet.get_status()
